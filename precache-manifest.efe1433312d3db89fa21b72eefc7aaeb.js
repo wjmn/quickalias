337b45d9276e6378421a54d1a21ec1ed38eb36da
wjmn <jamin.wu07@gmail.com>
2025-05-09 00:14:26 +0930

    Updates

--- precache-manifest.efe1433312d3db89fa21b72eefc7aaeb.js
+++ precache-manifest.efe1433312d3db89fa21b72eefc7aaeb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a528161f32034ed5d01b4435b98d2342",
+    "revision": "8da04a02a7b77696f6229fb8851f843d",
     "url": "https://wjmn.github.io/quickalias/index.html"
   },
   {
@@ -8,12 +8,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://wjmn.github.io/quickalias/manifest.json"
   },
   {
-    "revision": "17f094254e1bd73cda0e",
+    "revision": "1ec589687f5b919e0519",
     "url": "https://wjmn.github.io/quickalias/static/css/main.01e47e24.chunk.css"
   },
   {
-    "revision": "17f094254e1bd73cda0e",
-    "url": "https://wjmn.github.io/quickalias/static/js/main.17f09425.chunk.js"
+    "revision": "1ec589687f5b919e0519",
+    "url": "https://wjmn.github.io/quickalias/static/js/main.1ec58968.chunk.js"
   },
   {
     "revision": "8583b0e16bbcc9e7e1dc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://wjmn.github.io/quickalias/precache-manifest.dfa1a78f27b80b850a89cae806372fd6.js"
+  "https://wjmn.github.io/quickalias/precache-manifest.efe1433312d3db89fa21b72eefc7aaeb.js"
 );
 
 workbox.core.skipWaiting();
