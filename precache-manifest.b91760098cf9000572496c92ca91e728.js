79a4688d1e6ab82c8daad0a9a5da51b18d464e1d
wjmn <jamin.wu07@gmail.com>
2025-05-09 00:03:08 +0930

    Updates

---
+++ precache-manifest.b91760098cf9000572496c92ca91e728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b192c9de8141eb460a062f82b58a144e",
+    "url": "https://wjmn.github.io/quickalias/index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "https://wjmn.github.io/quickalias/manifest.json"
+  },
+  {
+    "revision": "b6d12c199e7ac531f174",
+    "url": "https://wjmn.github.io/quickalias/static/css/main.01e47e24.chunk.css"
+  },
+  {
+    "revision": "b6d12c199e7ac531f174",
+    "url": "https://wjmn.github.io/quickalias/static/js/main.b6d12c19.chunk.js"
+  },
+  {
+    "revision": "8583b0e16bbcc9e7e1dc",
+    "url": "https://wjmn.github.io/quickalias/static/js/runtime~main.8583b0e1.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "https://wjmn.github.io/quickalias/static/js/vendors~main.ded87515.chunk.js"
+  }
+]);
